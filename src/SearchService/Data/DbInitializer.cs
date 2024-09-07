ee2c8dd81c1a8fc5925e7ded103f49fa25b33252
saram4 <saravanan0982@gmail.com>
2024-09-08 01:20:16 +0530

    End of section 3

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text.Json;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data
 {
@@ -20,18 +21,15 @@ namespace SearchService.Data
 
             var count = await DB.CountAsync<Item>();
 
-            if (count == 0)
-            {
-                Console.WriteLine("No data - will attempt to seed");
+            using var scope = app.Services.CreateScope();
 
-                var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
 
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var items = await httpClient.GetItemsForSearchDb();
 
-                var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            Console.WriteLine(items.Count + " returned from the auction service");
 
-                await DB.SaveAsync(items);
-            }
+            if (items.Count > 0) await DB.SaveAsync(items);
         }
 
     }
